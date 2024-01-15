9d1a5fe8fe448f68472ee88bc8763e424cb4453a
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-01-15 03:02:36 +0300

    add base autoencoder from lecture

--- models.py
+++ models.py
@@ -9,10 +9,44 @@ import matplotlib.pyplot as plt
 import tqdm
 
 
+class Encoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(Encoder, self).__init__()
+        self.linear1 = nn.Linear(3*784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        return self.linear2(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(Decoder, self).__init__()
+        self.linear1 = nn.Linear(latent_dims, 512)
+        self.linear2 = nn.Linear(512, 3*784)
+
+    def forward(self, z):
+        z = F.relu(self.linear1(z))
+        z = torch.sigmoid(self.linear2(z))
+        return z.reshape((-1, 3, 28, 28))
+    
+
+class Autoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
+
 class VariationalEncoder(nn.Module):
     def __init__(self, latent_dims):
         super(VariationalEncoder, self).__init__()
-        self.linear1 = nn.Linear(784, 512)
+        self.linear1 = nn.Linear(3*784, 512)
         self.linear2 = nn.Linear(512, latent_dims)
         self.linear3 = nn.Linear(512, latent_dims)
 
@@ -29,18 +63,6 @@ class VariationalEncoder(nn.Module):
         z = mu + sigma*self.N.sample(mu.shape)
         self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
         return z
-
-
-class Decoder(nn.Module):
-    def __init__(self, latent_dims):
-        super(Decoder, self).__init__()
-        self.linear1 = nn.Linear(latent_dims, 512)
-        self.linear2 = nn.Linear(512, 784)
-
-    def forward(self, z):
-        z = F.relu(self.linear1(z))
-        z = torch.sigmoid(self.linear2(z))
-        return z.reshape((-1, 1, 28, 28))
  
 
 class VariationalAutoencoder(nn.Module):
